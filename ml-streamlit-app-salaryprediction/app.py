9c51afbe930f27d0556dcb200b718d23200967c7
wongwara.w <107904836+wongwara@users.noreply.github.com>
2023-05-05 00:35:47 +1000

    Update app.py

--- ml-streamlit-app-salaryprediction/app.py
+++ ml-streamlit-app-salaryprediction/app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from predict_page import show_predict_page
-from explore_page import show_explore_page
 
 # Page configuration
 st.set_page_config(
@@ -14,4 +13,5 @@ page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
 if page == "Predict":
     show_predict_page()
 else:
-    show_explore_page()
+    show_predict_page()
+    
