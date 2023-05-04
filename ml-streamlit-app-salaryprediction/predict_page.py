1977a79a0089744dd19aa60031d325036f7e7004
wongwara.w <107904836+wongwara@users.noreply.github.com>
2023-05-05 01:43:44 +1000

    Update predict_page.py

--- ml-streamlit-app-salaryprediction/predict_page.py
+++ ml-streamlit-app-salaryprediction/predict_page.py
@@ -146,15 +146,34 @@ def show_predict_page():
     
     ok = st.button("Calculate Salary")
     if ok:
-        X = np.array([[Jobclassification, isRightToWorkRequired, State,
-        Python,SQL,R,Tableau,SAS,Matlab,Hadoop,Spark,Java,Scala,recruiter,
-        teaser,desktopAdTemplate]])
-        X[:, 0] = Jobclassification.transform(X[:,0])
-        X['teaser'] = preprocess_text_input(teaser)
-        X['desktopAdTemplate'] = preprocess_text_input(desktopAdTemplate)
+        X = pd.DataFrame({
+        'JobClassification': [Jobclassification],
+        'IsRightToWorkRequired': [isRightToWorkRequired],
+        'State': [State],
+        'Python': [Python],
+        'SQL': [SQL],
+        'R': [R],
+        'Tableau': [Tableau],
+        'SAS': [SAS],
+        'Matlab': [Matlab],
+        'Hadoop': [Hadoop],
+        'Spark': [Spark],
+        'Java': [Java],
+        'Scala': [Scala],
+        'Recruiter': [recruiter],
+        'Teaser': [teaser],
+        'DesktopAdTemplate': [desktopAdTemplate]
+        })
+
+        X['JobClassification'] = Jobclassification.transform(X['JobClassification'])
+
+        X['Teaser'] = preprocess_text_input(X['Teaser'])
+        X['DesktopAdTemplate'] = preprocess_text_input(X['DesktopAdTemplate'])
+
         X = X.astype(str)
+
         salary = regressor.predict(X)
-        st.subheader(f"The estimated range salary is ${salary[0]:.2f}")
+        st.subheader(f"The estimated salary range is ${salary[0]:.2f}")
         st.write("'(100000.0, 110000.0] :0 ', '(90000.0, 100000.0] :1', '(110000.0, 120000.0] :2 ', '(80000.0, 90000.0] :3', '(130000.0, 140000.0] :4', '(60000.0, 80000.0] :5', '(120000.0, 130000.0] :6', '(140000.0, 160000.0] :7', '(180000.0, inf] :8', '(160000.0, 180000.0] :9', '(18000.0, 60000.0] :10' ")
         
 
